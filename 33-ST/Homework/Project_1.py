af052e1c884d68f2e17acb2ff6cdaa6b39569a93
Raquel Marques <raquelsmarques@gmail.com>
2026-07-04 20:11:01 -0400

    feat: ST Project data cleaning 1

--- 33-ST/Homework/Project_1.py
+++ 33-ST/Homework/Project_1.py
@@ -4,8 +4,10 @@
 
 # %%
 # LIBRARY
+# ------------------------------------------------------------------------------
 import os
 import pandas as pd
+from datetime import datetime
 
 
 
@@ -21,9 +23,56 @@ project_dir = os.path.join(os.path.expanduser("~"),
 
 # %%
 df = pd.read_csv(os.path.join(project_dir, "data", "Vendas ASN_Dados_Finais.csv"))
-df.head()
+
+print("Dataset Shape:", df.shape)
 
 # %%
+df.sample(10)
+
+
+# %%
+# Exploratory Data Analysis (EDA)
+# ------------------------------------------------------------------------------
+
+# raw data type
 df.info()
 
+# raw data range values
+df.describe()
+
 # %%
+# check for missing values
+df.isnull().sum()
+
+
+
+# %%
+# List unique categories
+print(df["Departamento"].unique())
+print(df["Seção"].unique())
+# print(df["Data_new"].unique())
+
+
+# %%
+## Data Cleaning & Preprocessing
+# ------------------------------------------
+
+# Variable : Data_new
+# Convert date from string to date
+df["Date"] = pd.to_datetime(df["Data_new"], errors='coerce')
+
+# Count total missing/invalid dates
+missing_count = df["Date"].isna().sum()
+print(f"Total missing or errored rows: {missing_count}")
+
+# Filter and display rows where the conversion failed
+error_rows = df[df["Date"].isna()]
+print(error_rows[["Data_new", "Date"]])
+
+# Check the earliest and latest dates to spot typos
+print(df["Date"].describe())
+
+
+# %%
+# Variable: Departamento
+
